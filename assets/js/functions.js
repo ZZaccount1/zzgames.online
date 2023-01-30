3143e43fc6156cdae04d328ede429c06c794c6bd
ZZ <nmio45137@gmail.com>
2023-01-30 19:41:47 +0200

    Remove unused function

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -10,43 +10,6 @@ function rand(arr) {
     return arr[Math.floor(Math.random() * arr.length)]
 }
 
-function uwu() {
-    document.getElementById("uwu").text = "OWO!!";
-    let emojis = [
-        "owo", "OwO",
-        "uwu", "UwU",
-        ":3", "x3",
-        "rawr x3",
-        "howls uwu",
-        "growl (╬ Ò ‸ Ó)",
-        "(* ^ ω ^)"
-    ];
-
-    let body = document.body.getElementsByTagName("main")[0];
-    let queue = [...body.children];
-    while (queue.length > 0) {
-        let element = queue.shift();
-        if (element.children.length > 0) {
-            queue.push(...element.children);
-        }
-        [...element.childNodes].filter((node) => {
-            return (node.nodeType === Node.TEXT_NODE && node.nodeValue.trim() !== "");
-        }).forEach((node) => {
-            node.textContent = node.textContent.replace(/[lr]/ig, (str) => { 
-                return str[0].toLowerCase() == str[0] ? "w" : "W";
-            }).replace(/[.!?]/gi, (str) => {
-                let char = str[0];
-                if (Math.random() > 0.7) {
-                    return char + " " + rand(emojis);
-                }
-                return char;
-            });
-        });
-    }
-    console.log(body);
-
-}
-
 window.onload=function() {
     let greetings = [
         "Hiya. Whatcha doing there? :eyes:",
