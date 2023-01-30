e1ccc9414f3f869cd463fea49e1e028c5c1cb589
ZZ <nmio45137@gmail.com>
2023-01-30 20:30:43 +0200

    move sidenav to right

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,8 +1,9 @@
 function openSidenav() {
     document.getElementById("sidenav").style.width = "100%";
+    console.log("open")
 }
-
+    
 function closeSidenav() {
     document.getElementById("sidenav").style.width = "0%";
+    console.log("close")
 }
-
